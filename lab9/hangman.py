d45bdf7eb4b72e0804e9efbb3239c27ee1165846
ofer hacohen <oferhac@post.bgu.ac.il>
2013-01-03 12:46:29 +0200

    added lab9

--- lab9/hangman.py
+++ lab9/hangman.py
@@ -1,9 +1,10 @@
 words = open('long-words.txt', 'rb')
 word = words.next()
 ind = len(word)
-while ind>-1
+while ind>-1:
 tmpchar = raw_input
-for letter in word
-if tmpchar = letter 
+for letter in word:
+if tmpchar = letter: 
 ind = ind - 1
-if ind = 0 print "success"
+if ind = 0:
+  print "success"
